3ebc85cdd8841dbb4ff4f10cef24dc7114ffc45e
arimusdev <143251520+arimusdev@users.noreply.github.com>
2024-02-09 17:32:34 +0800

    test:addItemModal

---
+++ src/components/AddItemModal/__tests__/index.tsx
@@ -0,0 +1,95 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import * as FormHooks from "react-hook-form";
+import { AddItemModal } from "..";
+import { colors } from "../../../utils/colors";
+
+jest.mock("~/stores/useModalStore", () => ({
+  ...jest.requireActual("~/stores/useModalStore"),
+  useModal: (id: string) => ({
+    close: jest.fn(),
+    open: jest.fn(),
+    visible: id === "addItem",
+  }),
+}));
+
+describe("AddItemModal", () => {
+  it("renders AddItemModal component correctly with correct props", async () => {
+    const { container } = render(<AddItemModal />);
+
+    const modal = container.querySelector("#addItem");
+
+    expect(modal).toBeInTheDocument();
+    expect(modal).toHaveAttribute("kind", "small");
+    expect(modal).toHaveStyle(`background: ${colors.BG1}`);
+  });
+
+  it("useForm hook is initialized with the correct default values and mode", async () => {
+    const mockFormHooks = jest.spyOn(FormHooks, "useForm");
+
+    const mockUseForm = FormHooks.useForm;
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    mockUseForm.mockImplementation(() => ({
+      control: jest.fn(),
+      watch: jest.fn(),
+      handleSubmit: jest.fn(),
+      reset: jest.fn(),
+    }));
+
+    render(<AddItemModal />);
+
+    await waitFor(() => {
+      expect(mockUseForm).toHaveBeenCalled();
+
+      expect(mockUseForm).toHaveBeenCalledWith(
+        expect.objectContaining({
+          mode: "onChange",
+        })
+      );
+    });
+
+    mockFormHooks.mockRestore();
+  });
+
+  it("navigates between steps correctly", async () => {
+    const { container } = render(<AddItemModal />);
+
+    function getButtonByText(text: "Prev" | "Next") {
+      return screen.getByText(text);
+    }
+
+    expect(screen.getByTestId("SourceTypeStep")).toBeInTheDocument();
+    expect(getButtonByText("Next")).toBeDisabled();
+
+    fireEvent.click(screen.getByText("Type"));
+    fireEvent.click(screen.getByText("Corporation"));
+
+    expect(getButtonByText("Next")).not.toBeDisabled();
+
+    fireEvent.click(getButtonByText("Next"));
+
+    expect(screen.queryByTestId("SourceStep")).toBeInTheDocument();
+    expect(getButtonByText("Prev")).toBeInTheDocument();
+
+    fireEvent.click(getButtonByText("Prev"));
+    expect(screen.getByTestId("SourceTypeStep")).toBeInTheDocument();
+
+    fireEvent.click(getButtonByText("Next"));
+
+    expect(getButtonByText("Next")).toBeDisabled();
+    await userEvent.type(container.querySelector("#cy-item-name")!, "testName");
+
+    expect(getButtonByText("Next")).not.toBeDisabled();
+    fireEvent.click(getButtonByText("Next"));
+
+    expect(screen.getByTestId("BudgetStep")).toBeInTheDocument();
+  });
+
+  // it('mock the API call to submit the form data and verify the form submission process', async () => {
+  //   expect('')
+  // })
+});
